d92cfc6f27f69007fe1a663f877ed3bb738dcba0
Charlie Bowman <c.b.bowman@outlook.com>
2023-01-21 13:05:44 -0500

    model tests

--- at2p/at2p_app/models.py
+++ at2p/at2p_app/models.py
@@ -41,66 +41,15 @@ class Crop(models.Model):
 
 
 class Planter(AbstractUser):
-    country = CountryField(default='US')
+    country = CountryField(default='US', blank=True, null=True)
     zip = models.CharField(max_length=10, blank=True, null=True)
     crops = models.ManyToManyField(Crop, through='TimeToPlant', blank=True)
 
-    def clean(self) -> None:
-        if self.zip is None:
-            return
-        n = pgeocode.Nominatim(self.country.code)
-        if self.country.code == n.query_postal_code(self.zip).country_code:
-            return
-        else:
-            raise ValidationError(
-                "Model error: Country / ZIP combination is invalid.")
-
     def __str__(self) -> str:
         return str(self.username)
 
     def __repr__(self) -> str:
         return str(self.username)
-
-
-class TimeToPlant(models.Model):
-    planter = models.ForeignKey(Planter, on_delete=models.CASCADE)
-    crop = models.ForeignKey(Crop, on_delete=models.CASCADE)
-    plantable = models.BooleanField(default=False)
-    chill = models.FloatField(blank=True, null=True)
-    cook = models.FloatField(blank=True, null=True)
-    soil = models.FloatField(blank=True, null=True)
-    plantable_score = models.FloatField(blank=True, null=True)
-    updated_on = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['-plantable_score', 'crop']
-
-    def __str__(self) -> str:
-        return self.planter + "'s " + self.crop
-
-    def __repr__(self) -> str:
-        return self.planter + "'s " + self.crop
-
-    def update_plantable(self, w):
-        mn = self.crop.min_temp
-        mx = self.crop.max_temp
-        ol = self.crop.min_opt_temp
-        oh = self.crop.max_opt_temp
-        low = w.forecast_low_temp
-        high = w.forecast_high_temp
-        soil = w.historic_avg_temp
-
-        chill = max((mn - low), 0) / max((ol - mn), 1)
-        cook = max((high - mx), 0) / max((mx - oh), 1)
-        sl = max(max(soil - oh, 0), max(ol - soil, 0)) / (oh - ol)
-        score = exp(-max(chill, cook, sl))
-
-        self.chill = chill
-        self.cook = cook
-        self.soil = sl
-        self.plantable_score = score
-        self.plantable = score == 1
-        self.save()
 
 
 class WeatherInfo(models.Model):
@@ -138,4 +87,45 @@ class WeatherInfo(models.Model):
         forecast = forecast_high_low(self.lat, self.long)
         self.forecast_high_temp = forecast[0]
         self.forecast_low_temp = forecast[1]
+        self.save()
+
+
+class TimeToPlant(models.Model):
+    planter = models.ForeignKey(Planter, on_delete=models.CASCADE)
+    crop = models.ForeignKey(Crop, on_delete=models.CASCADE)
+    plantable = models.BooleanField(default=False)
+    chill = models.FloatField(blank=True, null=True)
+    cook = models.FloatField(blank=True, null=True)
+    soil = models.FloatField(blank=True, null=True)
+    plantable_score = models.FloatField(blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-plantable_score', 'crop']
+
+    def __str__(self) -> str:
+        return self.planter + "'s " + self.crop
+
+    def __repr__(self) -> str:
+        return self.planter + "'s " + self.crop
+
+    def update_plantable(self, w):
+        mn = self.crop.min_temp
+        mx = self.crop.max_temp
+        ol = self.crop.min_opt_temp
+        oh = self.crop.max_opt_temp
+        low = w.forecast_low_temp
+        high = w.forecast_high_temp
+        soil = w.historic_avg_temp
+
+        chill = max((mn - low), 0) / max((ol - mn), 1)
+        cook = max((high - mx), 0) / max((mx - oh), 1)
+        sl = max(max(soil - oh, 0), max(ol - soil, 0)) / (oh - ol)
+        score = round(exp(-max(chill, cook, sl)), 2)
+
+        self.chill = chill
+        self.cook = cook
+        self.soil = sl
+        self.plantable_score = score
+        self.plantable = score == 1.00
         self.save()
--- at2p/at2p_app/tests/models_tests.py
+++ at2p/at2p_app/tests/models_tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from at2p_app.models import Crop, Planter
-from django.utils.text import slugify
+from at2p_app.models import Crop, Planter, WeatherInfo
+from django.core.exceptions import ValidationError
+import pgeocode
 
 
 class CropTest(TestCase):
@@ -22,14 +23,8 @@ class CropTest(TestCase):
                              min_opt_temp=min_opt_temp,
                              max_opt_temp=max_opt_temp)
         self.assertTrue(isinstance(c, Crop))
-        self.assertEqual(c.name, name)
         self.assertEqual(c.__str__(), name)
         self.assertEqual(c.__repr__(), name)
-        self.assertEqual(c.min_temp, min_temp)
-        self.assertEqual(c.max_temp, max_temp)
-        self.assertEqual(c.min_opt_temp, min_opt_temp)
-        self.assertEqual(c.max_opt_temp, max_opt_temp)
-        self.assertEqual(c.slug, slugify(name))
 
     def test_get_absolute_url(self):
         name = 'Boberries'
@@ -45,8 +40,8 @@ class CropTest(TestCase):
 
 class PlanterTest(TestCase):
 
-    def create_planter(self, username='rusty', password='b@A6&Zb!N&^W',
-                       zip='22405', country='US'):
+    def create_planter(self, zip, country, username='rusty',
+                       password='b@A6&Zb!N&^W'):
         return Planter.objects.create(username=username, password=password,
                                       zip=zip, country=country)
 
@@ -54,12 +49,33 @@ class PlanterTest(TestCase):
         username = 'shackleford'
         pw = 'b@A6&Zb!N&^W'
         zip = '22401'
-        country = 'MX'
+        country = 'US'
         p = self.create_planter(username=username, password=pw, zip=zip,
                                 country=country)
         self.assertTrue(isinstance(p, Planter))
-        self.assertEqual(p.username, username)
         self.assertEqual(p.__str__(), username)
         self.assertEqual(p.__repr__(), username)
-        self.assertEqual(p.zip, zip)
-        self.assertEqual(p.country, country)
+
+
+class WeatherInfoTest(TestCase):
+
+    def create_weather(self, avg, high, low, country, zip, lat, long):
+
+        return WeatherInfo.objects.create(historic_avg_temp=avg,
+                                          forecast_high_temp=high,
+                                          forecast_low_temp=low,
+                                          country=country, zip=zip, lat=lat,
+                                          long=long)
+
+    # def test_crop_creation(self):
+    #     name = 'Boberries'
+    #     min_temp = 0
+    #     max_temp = 100
+    #     min_opt_temp = 20
+    #     max_opt_temp = 80
+    #     c = self.create_crop(name=name, min_temp=min_temp, max_temp=max_temp,
+    #                          min_opt_temp=min_opt_temp,
+    #                          max_opt_temp=max_opt_temp)
+    #     self.assertTrue(isinstance(c, Crop))
+    #     self.assertEqual(c.__str_ _(), name)
+    #     self.assertEqual(c.__repr__(), name)
