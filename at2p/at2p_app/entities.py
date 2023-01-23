87c985ce6e773ea951152617aaa73188192af122
Charlie Bowman <c.b.bowman@outlook.com>
2023-01-23 16:42:38 -0500

    more tests

--- at2p/at2p_app/entities.py
+++ at2p/at2p_app/entities.py
@@ -1,10 +1,8 @@
-import string
-
-
 class TempRange:
     def __init__(self, minimum: int, maximum: int) -> None:
-        self.minimum = min(minimum, maximum)
-        self.maximum = max(minimum, maximum)
+        self.check_values(minimum, maximum)
+        self.minimum = int(round(min(minimum, maximum)))
+        self.maximum = int(round(max(minimum, maximum)))
 
     def __str__(self):
         min_str = "Minimum: " + str(self.minimum)
@@ -12,15 +10,25 @@ class TempRange:
         return min_str + "\n" + max_str
 
     def __repr__(self):
-        return self.__str__()
+        return (self.minimum, self.maximum)
+
+    def check_values(self, min: int, max: int) -> None:
+        min_type = type(min)
+        max_type = type(max)
+        min_is_num = min_type is int or min_type is float
+        max_is_num = max_type is int or max_type is float
+        values_are_nums = min_is_num and max_is_num
+        if not values_are_nums:
+            raise ValueError
+        return
 
 
 class Crop:
     def __init__(
         self, name: str, abs_temp: TempRange, opt_temp: TempRange
     ) -> None:
+        self.check_values(name, abs_temp, opt_temp)
         self.name = name
-        self.check_temp_ranges(abs_temp, opt_temp)
         self.abs_temp = abs_temp
         self.opt_temp = opt_temp
 
@@ -29,6 +37,11 @@ class Crop:
 
     def __repr__(self):
         return self.__str__()
+
+    def check_values(self, name, abs_temp, opt_temp) -> None:
+        self.check_name(name)
+        self.check_temp_ranges(abs_temp, opt_temp)
+        return
 
     def check_temp_ranges(
         self, abs_temp: TempRange, opt_temp: TempRange
@@ -41,9 +54,18 @@ class Crop:
             raise ValueError(error_msg)
         return
 
+    def check_name(self, name: str) -> None:
+        error_msg = "Name may not be blank or none!"
+        name_is_blank = name == ""
+        name_is_none = name == None
+        if name_is_none or name_is_blank:
+            raise ValueError(error_msg)
+        return
+
 
 class Country:
     def __init__(self, full_name: str, code: str) -> None:
+        self.check_values(full_name, code)
         self.full_name = full_name.title()
         self.code = code.upper()
 
@@ -53,22 +75,75 @@ class Country:
     def __repr__(self):
         return self.code
 
+    def check_values(self, full_name: str, code: str) -> None:
+        self.check_name(full_name)
+        self.check_code(code)
+        return
+
+    def check_name(self, full_name: str) -> None:
+        error_msg = "Country name must be a string!"
+        name_is_string = type(full_name) == str
+        if not name_is_string:
+            raise ValueError(error_msg)
+        return
+
+    def check_code(self, code: str) -> None:
+        error_msg = "Country code must be a two character string!"
+        code_is_string = type(code) == str
+        if not code_is_string:
+            raise ValueError(error_msg)
+
+        code_length = 2
+        if len(code) != code_length:
+            raise ValueError(error_msg)
+
+        return
+
 
 class LatLong:
     lat: float
     long: float
 
     def __init__(self, lat: float, long: float) -> None:
+        self.check_values(lat, long)
         self.lat = float(lat)
         self.long = float(long)
 
     def __str__(self):
-        str_lat = f'Latitude: {self.lat:.4f}'
-        str_long = f'Longitude: {self.long:.4f}'
-        return str_lat + '\n' + str_long
+        str_lat = f"Latitude: {self.lat:.4f}"
+        str_long = f"Longitude: {self.long:.4f}"
+        return str_lat + "\n" + str_long
 
     def __repr__(self):
         return (self.lat, self.long)
+
+    def check_values(self, lat: float, long: float) -> None:
+        self.check_lat(lat)
+        self.check_long(long)
+
+    def check_lat(self, lat: float) -> None:
+        error_msg = "Latitude must be a number between -90 and 90"
+        if self.is_a_number(lat):
+            raise ValueError(error_msg)
+        print(lat < 90)
+        print(lat > -90)
+        lat_ok = lat < 90 and lat > -90
+        if not lat_ok:
+            raise ValueError(error_msg)
+        return
+
+    def check_long(self, long: float) -> None:
+        error_msg = "Longitude must be a number between -180 and 180"
+        if self.is_a_number(long):
+            raise ValueError(error_msg)
+        long_ok = long < 180 and long > -180
+        if not long_ok:
+            raise ValueError(error_msg)
+
+    def is_a_number(self, value) -> bool:
+        val_type = type(value)
+        return val_type in [int, float]
+
 
 
 class Place:
--- at2p/at2p_app/tests/entities_tests.py
+++ at2p/at2p_app/tests/entities_tests.py
@@ -4,11 +4,19 @@ from django.test import TestCase
 
 class TestTempRange(TestCase):
     def test_create_temp_range(self):
+
         tr = TempRange(10, 20)
         self.assertTrue(isinstance(tr, TempRange))
-        tr_str = "Minimum: 10\nMaximum: 20"
+        tr_str = 'Minimum: 10\nMaximum: 20'
         self.assertEqual(tr.__str__(), tr_str)
-        self.assertEqual(tr.__repr__(), tr_str)
+        self.assertEqual(tr.__repr__(), (10, 20))
+        self.assertRaises(ValueError, TempRange, minimum='A', maximum=None)
+
+    def test_value_cleaning(self):
+
+        tr = TempRange(9.5, 20.2)
+        self.assertEqual(tr.__repr__(), (10, 20))
+        self.assertRaises(ValueError, TempRange, minimum='A', maximum=None)
 
     def test_correct_order(self):
 
@@ -27,18 +35,31 @@ class TestTempRange(TestCase):
 
 class CropTest(TestCase):
     def test_create_crop(self):
-        name = "Boberries"
-        abs_temp = TempRange(10, 60)
-        opt_temp = TempRange(30, 40)
-        c = Crop(name, abs_temp, opt_temp)
+        name = 'Boberries'
+        abs = TempRange(10, 60)
+        opt = TempRange(30, 40)
+        c = Crop(name, abs, opt)
         self.assertTrue(isinstance(c, Crop))
 
         c_str = name
         self.assertEqual(c.__str__(), c_str)
         self.assertEqual(c.__repr__(), c_str)
 
+    def test_value_errors(self) -> None:
+        name = None
+        abs = TempRange(10, 60)
+        opt = TempRange(30, 40)
+        self.assertRaises(
+            ValueError, Crop, name=name, abs_temp=abs, opt_temp=opt
+        )
+
+        name = ''
+        self.assertRaises(
+            ValueError, Crop, name=name, abs_temp=abs, opt_temp=opt
+        )
+
     def test_check_temp_ranges(self):
-        name = "Boberries"
+        name = 'Boberries'
         abs_temp = TempRange(30, 60)
         opt_temp = TempRange(10, 40)
         self.assertRaises(
@@ -48,22 +69,40 @@ class CropTest(TestCase):
 
 class CountryTest(TestCase):
     def test_create_country(self):
-        full_name = "greece"
-        code = "gr"
+        full_name = 'greece'
+        code = 'gr'
         c = Country(full_name=full_name, code=code)
         self.assertTrue(isinstance(c, Country))
 
-        c_str = "Greece"
-        c_repr = "GR"
+        c_str = 'Greece'
+        c_repr = 'GR'
         self.assertEqual(c.__str__(), c_str)
         self.assertEqual(c.__repr__(), c_repr)
+
+    def test_value_errors(self):
+        name = ''
+        code = None
+        self.assertRaises(ValueError, Country, full_name=name, code=code)
+        
+        name = 'Greece'
+        code = 'grc'
+        self.assertRaises(ValueError, Country, full_name=name, code=code)
 
 
 class LatLongTest(TestCase):
     def test_create_lat_long(self):
         lat_long = LatLong(10, 20)
         self.assertTrue(isinstance(lat_long, LatLong))
-        lat_long_str = "Latitude: 10.0000\nLongitude: 20.0000"
+        lat_long_str = 'Latitude: 10.0000\nLongitude: 20.0000'
         lat_long_repr = (10.0, 20.0)
         self.assertEqual(lat_long.__str__(), lat_long_str)
         self.assertEqual(lat_long.__repr__(), lat_long_repr)
+
+    def test_check_values(self):
+        lat = 100
+        long = 200
+        self.assertRaises(ValueError, LatLong, lat=lat, long=long)
+
+        lat = 'latitude'
+        long = None
+        self.assertRaises(ValueError, LatLong, lat=lat, long=long)
