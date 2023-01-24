36d6522552abe8e1d6aed52d3201c57a1c2d1602
Charlie Bowman <c.b.bowman@outlook.com>
2023-01-24 16:48:31 -0500

    dataclasses and testing

--- at2p/at2p_app/entities.py
+++ at2p/at2p_app/entities.py
@@ -1,25 +1,67 @@
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+
+@dataclass(slots=True, order=True, eq=True)
+class Temp:
+    '''Class for temperate value'''
+
+    temp: float
+    scale: str = 'F'
+
+    def __str__(self) -> str:
+        return f'{self.temp:.1F} \u00B0{self.scale}'
+
+    def __post_init__(self):
+        if not isinstance(self.temp, (int, float)):
+            raise ValueError('Temp must be a number!')
+
+
+@dataclass(slots=True, kw_only=True)
 class TempRange:
-    def __init__(self, minimum: int, maximum: int) -> None:
-        self.check_values(minimum, maximum)
-        self.minimum = int(round(min(minimum, maximum)))
-        self.maximum = int(round(max(minimum, maximum)))
+    '''Class for a range of temperatures'''
 
-    def __str__(self):
-        min_str = "Minimum: " + str(self.minimum)
-        max_str = "Maximum: " + str(self.maximum)
-        return min_str + "\n" + max_str
+    min: Temp
+    max: Temp
 
-    def __repr__(self):
-        return (self.minimum, self.maximum)
+    def __post_init__(self) -> None:
+        if not self.validate():
+            raise ValueError('Temp range incorrect')
+        return
 
-    def check_values(self, min: int, max: int) -> None:
-        min_type = type(min)
-        max_type = type(max)
-        min_is_num = min_type is int or min_type is float
-        max_is_num = max_type is int or max_type is float
-        values_are_nums = min_is_num and max_is_num
-        if not values_are_nums:
-            raise ValueError
+    def validate(self) -> bool:
+        if not isinstance(self.min, Temp):
+            return False
+        if not isinstance(self.max, Temp):
+            return False
+        if self.min > self.max:
+            return False
+        return True
+
+    def __str__(self) -> str:
+        return f'{self.min} \u2013 {self.max}'
+
+@dataclass
+class AbstractRequirementList(ABC):
+    requirements: list
+
+    @abstractmethod
+    def _validate_requirements(self):
+        pass
+
+    def __init__(self) -> None:
+        self._validate_requirements()
+        super().__init__()
+
+class TempReqList(AbstractRequirementList):
+    absolute_range: TempRange
+    optimal_range: TempRange
+
+    def _validate_requirements(self):
+        mins_wrong = self.absolute_range.min > self.optimal_range.min
+        maxs_wrong = self.absolute_range.max < self.optimal_range.max
+        if mins_wrong or maxs_wrong:
+            raise ValueError('Temp ranges are incorrrect')
         return
 
 
@@ -44,8 +86,8 @@ class Crop:
         return
 
     def check_name(self, name: str) -> None:
-        error_msg = "Name may not be blank or none!"
-        name_is_blank = name == ""
+        error_msg = 'Name may not be blank or none!'
+        name_is_blank = name == ''
         name_is_none = name is None
         if name_is_none or name_is_blank:
             raise ValueError(error_msg)
@@ -54,9 +96,9 @@ class Crop:
     def check_temp_ranges(
         self, abs_temp: TempRange, opt_temp: TempRange
     ) -> None:
-        error_msg = "Optimal range must be inside absolute range!"
-        min_wrong = abs_temp.minimum > opt_temp.minimum
-        max_wrong = abs_temp.maximum < opt_temp.maximum
+        error_msg = 'Optimal range must be inside absolute range!'
+        min_wrong = abs_temp.min > opt_temp.min
+        max_wrong = abs_temp.max < opt_temp.max
         opt_not_within_abs = min_wrong or max_wrong
         if opt_not_within_abs:
             raise ValueError(error_msg)
@@ -81,18 +123,18 @@ class Country:
         return
 
     def check_name(self, full_name: str) -> None:
-        error_msg = "Country name must be a string!"
+        error_msg = 'Country name must be a string!'
         name_is_string = type(full_name) == str
         if not name_is_string:
             raise ValueError(error_msg)
 
-        error_msg = "Country name must not be blank"
+        error_msg = 'Country name must not be blank'
         if len(full_name) == 0:
             raise ValueError(error_msg)
         return
 
     def check_code(self, code: str) -> None:
-        error_msg = "Country code must be a two character string!"
+        error_msg = 'Country code must be a two character string!'
         code_is_string = type(code) == str
         if not code_is_string:
             raise ValueError(error_msg)
@@ -111,9 +153,9 @@ class LatLong:
         self.long = float(long)
 
     def __str__(self):
-        str_lat = f"Latitude: {self.lat:.4f}"
-        str_long = f"Longitude: {self.long:.4f}"
-        return str_lat + "\n" + str_long
+        str_lat = f'Latitude: {self.lat:.4f}'
+        str_long = f'Longitude: {self.long:.4f}'
+        return str_lat + '\n' + str_long
 
     def __repr__(self):
         return (self.lat, self.long)
@@ -123,7 +165,7 @@ class LatLong:
         self.check_long(long)
 
     def check_lat(self, lat: float) -> None:
-        error_msg = "Latitude must be a number between -90 and 90"
+        error_msg = 'Latitude must be a number between -90 and 90'
         if not self.is_a_number(lat):
             raise ValueError(error_msg)
         lat_ok = lat < 90 and lat > -90
@@ -132,7 +174,7 @@ class LatLong:
         return
 
     def check_long(self, long: float) -> None:
-        error_msg = "Longitude must be a number between -180 and 180"
+        error_msg = 'Longitude must be a number between -180 and 180'
         if not self.is_a_number(long):
             raise ValueError(error_msg)
         long_ok = long < 180 and long > -180
@@ -151,7 +193,7 @@ class Place:
         city: str,
         zip: str,
         coord: LatLong,
-        elev: int
+        elev: int,
     ) -> None:
 
         self.check_values(country, state, city, zip, coord, elev)
@@ -164,7 +206,7 @@ class Place:
         self.elev = elev
 
     def __str__(self) -> str:
-        return f"{self.city}, {self.state}, {self.country.code}"
+        return f'{self.city}, {self.state}, {self.country.code}'
 
     def __repr__(self) -> str:
         return (self.country.code, self.zip)
@@ -176,7 +218,7 @@ class Place:
         city: str,
         zip: str,
         coord: LatLong,
-        elev: int
+        elev: int,
     ) -> None:
         if not isinstance(country, Country):
             raise ValueError()
--- at2p/at2p_app/tests/entities_tests.py
+++ at2p/at2p_app/tests/entities_tests.py
@@ -1,217 +1,199 @@
-from at2p_app.entities import TempRange, Crop, Country, LatLong, Place
+from at2p_app.entities import TempRange, Crop, Country, LatLong, Place, Temp
 from django.test import TestCase
 
 
-class TempRangeFactory:
-    def _get_temp_range(self, min: int = 1, max: int = 100) -> TempRange:
-        return TempRange(min, max)
+# class TempRangeFactory:
+#     def _get_temp_range(self, min: int = 1, max: int = 100) -> TempRange:
+#         return TempRange(min, max)
 
 
 class TestTempRange(TestCase):
     def test_create_temp_range(self) -> None:
-        tr_fact = TempRangeFactory()
-        min = 1
-        max = 100
-        tr = tr_fact._get_temp_range(min, max)
+        # tr_fact = TempRangeFactory()
+        min = Temp(1.0)
+        max = Temp(100)
+        tr = TempRange(min=min, max=max)
         self.assertTrue(isinstance(tr, TempRange))
 
-        tr_str = f"Minimum: {min}\nMaximum: {max}"
+        tr_str = f'{min} \u2013 {max}'
         self.assertEqual(tr.__str__(), tr_str)
 
-        tr_repr = (min, max)
-        self.assertEqual(tr.__repr__(), tr_repr)
-
     def test_value_cleaning(self) -> None:
-        tr_fact = TempRangeFactory()
-        min = 9.5
-        max = 20.2
-        tr = tr_fact._get_temp_range(min, max)
-        tr_repr = (10, 20)
-        self.assertEqual(tr.__repr__(), tr_repr)
-
-        max = "Not an integer"
-        self.assertRaises(ValueError, tr_fact._get_temp_range, max)
-
+        max = 'Not an integer'
+        self.assertRaises(ValueError, Temp, max)
         min = None
-        self.assertRaises(ValueError, tr_fact._get_temp_range, min)
+        self.assertRaises(ValueError, Temp, min)
 
     def test_correct_order(self) -> None:
-        larger = 100
-        smaller = 1
-        tr_fact = TempRangeFactory()
-        tr = tr_fact._get_temp_range(larger, smaller)
-        self.assertEqual(tr.minimum, smaller)
-        self.assertEqual(tr.maximum, larger)
-
-        tr = tr_fact._get_temp_range(smaller, smaller)
-        self.assertEqual(tr.minimum, smaller)
-        self.assertEqual(tr.maximum, smaller)
+        larger = Temp(100)
+        smaller = Temp(1)
+        self.assertRaises(ValueError, TempRange, min=larger, max=smaller)
 
 
-class CropFactory:
-    def _get_crop(
-        self,
-        name: str = "Boberries",
-        abs: TempRange = TempRange(1, 100),
-        opt: TempRange = TempRange(45, 55),
-    ) -> Crop:
-        return Crop(name, abs, opt)
+# class CropFactory:
+#     def _get_crop(
+#         self,
+#         name: str = 'Boberries',
+#         abs: TempRange = TempRange(min=Temp(1), max=Temp(100)),
+#         opt: TempRange = TempRange(min=Temp(45), max=Temp(55)),
+#     ) -> Crop:
+#         return Crop(name, abs, opt)
 
 
-class CropTest(TestCase):
-    def test_create_crop(self) -> None:
-        c_fact = CropFactory()
-        c = c_fact._get_crop()
-        self.assertTrue(isinstance(c, Crop))
+# class CropTest(TestCase):
+#     def test_create_crop(self) -> None:
+#         c_fact = CropFactory()
+#         c = c_fact._get_crop()
+#         self.assertTrue(isinstance(c, Crop))
 
-        c_str = c.name
-        c_repr = c.name
-        self.assertEqual(c.__str__(), c_str)
-        self.assertEqual(c.__repr__(), c_repr)
+#         c_str = c.name
+#         c_repr = c.name
+#         self.assertEqual(c.__str__(), c_str)
+#         self.assertEqual(c.__repr__(), c_repr)
 
-    def test_value_errors(self) -> None:
-        c_fact = CropFactory()
-        name = None
-        self.assertRaises(ValueError, c_fact._get_crop, name=name)
+#     def test_value_errors(self) -> None:
+#         c_fact = CropFactory()
+#         name = None
+#         self.assertRaises(ValueError, c_fact._get_crop, name=name)
 
-        name = ""
-        self.assertRaises(ValueError, c_fact._get_crop, name=name)
+#         name = ''
+#         self.assertRaises(ValueError, c_fact._get_crop, name=name)
 
-    def test_check_temp_ranges(self) -> None:
-        c_fact = CropFactory()
-        abs = TempRange(30, 60)
-        opt = TempRange(10, 40)
-        self.assertRaises(ValueError, c_fact._get_crop, abs=abs, opt=opt)
-
-
-class CountryFactory:
-    def _get_country(self, name: str = "Greece", code: str = "GR") -> Country:
-        return Country(name, code)
+#     def test_check_temp_ranges(self) -> None:
+#         c_fact = CropFactory()
+#         abs = TempRange(min=Temp(30), max=Temp(60))
+#         opt = TempRange(min=Temp(10), max=Temp(40))
+#         self.assertRaises(ValueError, c_fact._get_crop, abs=abs, opt=opt)
 
 
-class CountryTest(TestCase):
-    def test_create_country(self) -> None:
-        c_fact = CountryFactory()
-        c = c_fact._get_country()
-        self.assertTrue(isinstance(c, Country))
-
-        c_str = c.full_name
-        c_repr = c.code
-        self.assertEqual(c.__str__(), c_str)
-        self.assertEqual(c.__repr__(), c_repr)
-
-    def test_value_errors(self) -> None:
-        c_fact = CountryFactory()
-        name = ""
-        self.assertRaises(ValueError, c_fact._get_country, name=name)
-
-        name = None
-        self.assertRaises(ValueError, c_fact._get_country, name=name)
-
-        code = "grc"
-        self.assertRaises(ValueError, c_fact._get_country, code=code)
-
-        code = None
-        self.assertRaises(ValueError, c_fact._get_country, code=code)
+# class CountryFactory:
+#     def _get_country(self, name: str = 'Greece', code: str = 'GR') -> Country:
+#         return Country(name, code)
 
 
-class LatLongFactory:
-    def _get_lat_long(self, lat: float = 10.0, long: float = 10.0) -> LatLong:
-        return LatLong(lat, long)
+# class CountryTest(TestCase):
+#     def test_create_country(self) -> None:
+#         c_fact = CountryFactory()
+#         c = c_fact._get_country()
+#         self.assertTrue(isinstance(c, Country))
+
+#         c_str = c.full_name
+#         c_repr = c.code
+#         self.assertEqual(c.__str__(), c_str)
+#         self.assertEqual(c.__repr__(), c_repr)
+
+#     def test_value_errors(self) -> None:
+#         c_fact = CountryFactory()
+#         name = ''
+#         self.assertRaises(ValueError, c_fact._get_country, name=name)
+
+#         name = None
+#         self.assertRaises(ValueError, c_fact._get_country, name=name)
+
+#         code = 'grc'
+#         self.assertRaises(ValueError, c_fact._get_country, code=code)
+
+#         code = None
+#         self.assertRaises(ValueError, c_fact._get_country, code=code)
 
 
-class LatLongTest(TestCase):
-    def test_create_lat_long(self) -> None:
-        ll_fact = LatLongFactory()
-        ll = ll_fact._get_lat_long()
-        self.assertTrue(isinstance(ll, LatLong))
-
-        ll_str = f"Latitude: {ll.lat:.4f}\nLongitude: {ll.long:.4f}"
-        ll_repr = (ll.lat, ll.long)
-        self.assertEqual(ll.__str__(), ll_str)
-        self.assertEqual(ll.__repr__(), ll_repr)
-
-    def test_check_values(self) -> None:
-        ll_fact = LatLongFactory()
-        lat = 100
-        self.assertRaises(ValueError, ll_fact._get_lat_long, lat=lat)
-
-        lat = "latitude"
-        self.assertRaises(ValueError, ll_fact._get_lat_long, lat=lat)
-
-        lat = None
-        self.assertRaises(ValueError, ll_fact._get_lat_long, lat=lat)
-
-        long = 200
-        self.assertRaises(ValueError, ll_fact._get_lat_long, long=long)
-
-        long = "Longitude"
-        self.assertRaises(ValueError, ll_fact._get_lat_long, long=long)
-
-        long = None
-        self.assertRaises(ValueError, ll_fact._get_lat_long, long=long)
+# class LatLongFactory:
+#     def _get_lat_long(self, lat: float = 10.0, long: float = 10.0) -> LatLong:
+#         return LatLong(lat, long)
 
 
-class PlaceFactory:
-    def _get_place(
-        self,
-        country: Country = Country("Greece", "GR"),
-        state: str = "Virginia",
-        city: str = "Fredericksburg",
-        zip: str = "22401",
-        latlong: LatLong = LatLong(0, 0),
-        elev: int = 100,
-    ) -> Place:
-        return Place(country, state, city, zip, latlong, elev)
+# class LatLongTest(TestCase):
+#     def test_create_lat_long(self) -> None:
+#         ll_fact = LatLongFactory()
+#         ll = ll_fact._get_lat_long()
+#         self.assertTrue(isinstance(ll, LatLong))
+
+#         ll_str = f'Latitude: {ll.lat:.4f}\nLongitude: {ll.long:.4f}'
+#         ll_repr = (ll.lat, ll.long)
+#         self.assertEqual(ll.__str__(), ll_str)
+#         self.assertEqual(ll.__repr__(), ll_repr)
+
+#     def test_check_values(self) -> None:
+#         ll_fact = LatLongFactory()
+#         lat = 100
+#         self.assertRaises(ValueError, ll_fact._get_lat_long, lat=lat)
+
+#         lat = 'latitude'
+#         self.assertRaises(ValueError, ll_fact._get_lat_long, lat=lat)
+
+#         lat = None
+#         self.assertRaises(ValueError, ll_fact._get_lat_long, lat=lat)
+
+#         long = 200
+#         self.assertRaises(ValueError, ll_fact._get_lat_long, long=long)
+
+#         long = 'Longitude'
+#         self.assertRaises(ValueError, ll_fact._get_lat_long, long=long)
+
+#         long = None
+#         self.assertRaises(ValueError, ll_fact._get_lat_long, long=long)
 
 
-class PlaceTest(TestCase):
-    def test_create_place(self) -> None:
-        p_fact = PlaceFactory()
-        p = p_fact._get_place()
-        p_str = f"{p.city}, {p.state}, {p.country.code}"
-        p_repr = (p.country.code, p.zip)
-        self.assertIsInstance(p, Place)
-        self.assertEqual(p.__str__(), p_str)
-        self.assertEqual(p.__repr__(), p_repr)
-        return
+# class PlaceFactory:
+#     def _get_place(
+#         self,
+#         country: Country = Country('Greece', 'GR'),
+#         state: str = 'Virginia',
+#         city: str = 'Fredericksburg',
+#         zip: str = '22401',
+#         latlong: LatLong = LatLong(0, 0),
+#         elev: int = 100,
+#     ) -> Place:
+#         return Place(country, state, city, zip, latlong, elev)
 
-    def test_check_str_values(self) -> None:
-        p_fact = PlaceFactory()
 
-        bad_value = 12
-        self.assertRaises(ValueError, p_fact._get_place, state=bad_value)
-        bad_value = True
-        self.assertRaises(ValueError, p_fact._get_place, state=bad_value)
-        bad_value = None
-        self.assertRaises(ValueError, p_fact._get_place, state=bad_value)
-        bad_value = ""
-        self.assertRaises(ValueError, p_fact._get_place, state=bad_value)
+# class PlaceTest(TestCase):
+#     def test_create_place(self) -> None:
+#         p_fact = PlaceFactory()
+#         p = p_fact._get_place()
+#         p_str = f'{p.city}, {p.state}, {p.country.code}'
+#         p_repr = (p.country.code, p.zip)
+#         self.assertIsInstance(p, Place)
+#         self.assertEqual(p.__str__(), p_str)
+#         self.assertEqual(p.__repr__(), p_repr)
+#         return
 
-        bad_value = 12
-        self.assertRaises(ValueError, p_fact._get_place, city=bad_value)
-        bad_value = True
-        self.assertRaises(ValueError, p_fact._get_place, city=bad_value)
-        bad_value = None
-        self.assertRaises(ValueError, p_fact._get_place, city=bad_value)
-        bad_value = ""
-        self.assertRaises(ValueError, p_fact._get_place, city=bad_value)
+#     def test_check_str_values(self) -> None:
+#         p_fact = PlaceFactory()
 
-        bad_value = 12
-        self.assertRaises(ValueError, p_fact._get_place, zip=bad_value)
-        bad_value = True
-        self.assertRaises(ValueError, p_fact._get_place, zip=bad_value)
-        bad_value = None
-        self.assertRaises(ValueError, p_fact._get_place, zip=bad_value)
-        bad_value = ""
-        self.assertRaises(ValueError, p_fact._get_place, zip=bad_value)
+#         bad_value = 12
+#         self.assertRaises(ValueError, p_fact._get_place, state=bad_value)
+#         bad_value = True
+#         self.assertRaises(ValueError, p_fact._get_place, state=bad_value)
+#         bad_value = None
+#         self.assertRaises(ValueError, p_fact._get_place, state=bad_value)
+#         bad_value = ''
+#         self.assertRaises(ValueError, p_fact._get_place, state=bad_value)
 
-    def test_correct_types(self) -> None:
-        p_fact = PlaceFactory()
+#         bad_value = 12
+#         self.assertRaises(ValueError, p_fact._get_place, city=bad_value)
+#         bad_value = True
+#         self.assertRaises(ValueError, p_fact._get_place, city=bad_value)
+#         bad_value = None
+#         self.assertRaises(ValueError, p_fact._get_place, city=bad_value)
+#         bad_value = ''
+#         self.assertRaises(ValueError, p_fact._get_place, city=bad_value)
 
-        bad_value = 0
-        self.assertRaises(ValueError, p_fact._get_place, country=bad_value)
-        self.assertRaises(ValueError, p_fact._get_place, latlong=bad_value)
+#         bad_value = 12
+#         self.assertRaises(ValueError, p_fact._get_place, zip=bad_value)
+#         bad_value = True
+#         self.assertRaises(ValueError, p_fact._get_place, zip=bad_value)
+#         bad_value = None
+#         self.assertRaises(ValueError, p_fact._get_place, zip=bad_value)
+#         bad_value = ''
+#         self.assertRaises(ValueError, p_fact._get_place, zip=bad_value)
 
-        bad_value = "Not an integer"
-        self.assertRaises(ValueError, p_fact._get_place, elev=bad_value)
+#     def test_correct_types(self) -> None:
+#         p_fact = PlaceFactory()
+
+#         bad_value = 0
+#         self.assertRaises(ValueError, p_fact._get_place, country=bad_value)
+#         self.assertRaises(ValueError, p_fact._get_place, latlong=bad_value)
+
+#         bad_value = 'Not an integer'
+#         self.assertRaises(ValueError, p_fact._get_place, elev=bad_value)
