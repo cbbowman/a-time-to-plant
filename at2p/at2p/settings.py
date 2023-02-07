11a8de080e0f5c94e9f79b014695200c865d19ca
Charlie Bowman <c.b.bowman@outlook.com>
2023-02-07 12:50:49 -0500

    change allowed hosts

--- at2p/at2p/settings.py
+++ at2p/at2p/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = str(os.environ['SECRET_KEY'])
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = strtobool(os.environ['DJANGO_DEBUG'])
 
-ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS']
+# ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS']
+ALLOWED_HOSTS = ['157.230.84.7', 'localhost']
 
 
 # Application definition
